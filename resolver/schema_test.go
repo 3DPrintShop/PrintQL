e1d9984f2d1f632e93b2ac8bab786e75846b4259
Vitiock <vitiock@gmail.com>
2020-06-12 16:56:57 -0700

    Fix unit test

--- resolver/schema_test.go
+++ resolver/schema_test.go
@@ -1,25 +1,10 @@
 package resolver
 
 import (
-	"context"
 	"github.com/3DPrintShop/PrintQL/schema"
-	"github.com/graph-gophers/dataloader"
 	"github.com/graph-gophers/graphql-go"
-	"net/http"
 	"testing"
 )
-
-func setup(loaderId key, loader dataloader.BatchFunc) (context.Context, error) {
-	request, err := http.NewRequest("POST", "/graphql", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx := request.Context()
-	ctx = context.WithValue(ctx, loaderId, dataloader.NewBatchedLoader(loader))
-
-	return ctx, nil
-}
 
 func TestResolversSatisfySchema(t *testing.T) {
 	rootResolver := &SchemaResolver{}
